b59184822cd09e5c488994016b0dd322b5caffd6
Arvin Mahmoudi <mahmoudi.arvin@gmail.com>
2020-04-02 00:49:42 -0400

    adding mongoose schema and create html routes

--- models/workout.js
+++ models/workout.js
@@ -1,1 +1,45 @@
 const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const data = {
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            require: " Exercies type cannot be empty !",
+            trim: true
+        },
+        name: {
+            type: String,
+            require: "Exercies name cannot be empty !",
+            trim: true
+        },
+        duration: {
+            type: Number,
+            require: "Exercies duration cannot be empty !",
+            trim: true
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        }
+
+    }]
+
+
+};
+
+const workoutSchema = new Schema(data);
+const Workout = mongoose.model("Workout",workoutSchema);
+
+module.exports=Workout;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express=require("express");
 const app=express();
-
 const PORT =process.env.PORT|| 7000;
-
 const mongoose = require("mongoose");
 const logger = require("morgan");
 
@@ -14,6 +12,9 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
+
+app.use(require("./routes/html_routes.js"));
+
 
 
 app.listen(PORT,()=>{
